73e1981121830cdbaef153a9f90e1c27f61962c9
ernest-lu <53326307+ernest-lu@users.noreply.github.com>
2023-07-15 17:38:14 -0700

    Delete .firebase directory

--- .firebase/bintbadint/functions/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.0a2d02c1.js","_app/immutable/chunks/scheduler.afdc9194.js","_app/immutable/chunks/index.a17f34e5.js","_app/immutable/chunks/singletons.8ad10468.js"];
-export const stylesheets = [];
-export const fonts = [];
